4267d6289909a62f975b9aff3d2933e2be11b083
João Pedro <154932739+joaopaseixas@users.noreply.github.com>
2024-07-14 12:58:53 -0300

    Add files via upload

---
+++ principal.js
@@ -0,0 +1,16 @@
+const wrapper = document.querySelector('.wrapper')
+const pergunta = document.querySelector('.pergunta')
+const btnSim = document.querySelector('.btn-sim')
+const btnNao = document.querySelector('.btn-nao')
+
+const wrapperRect = wrapper.getBoundingClientRect();
+const btnNaoRect = btnNao.getBoundingClientRect();
+
+
+btnNao.addEventListener('mouseover', () => {
+  const i = Math.floor(Math.random() * (wrapperRect.width - btnNaoRect.width)) +1;
+  const j = Math.floor(Math.random() * (wrapperRect.height - btnNaoRect.height)) +1;
+
+  btnNao.style.left = i + 'px';
+  btnNao.style.top = j + 'px';
+});
